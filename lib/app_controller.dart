a8ecc6f2a8ed63a8491015b0dcc3d4c4f313f9bc
Guilherme Deitos <guilherme.cascavel@gmail.com>
2024-03-15 18:26:49 -0300

    Calculadora de idade (Sim kkkkkkk)

--- lib/app_controller.dart
+++ lib/app_controller.dart
@@ -8,3 +8,61 @@ class AppController extends ChangeNotifier{
     notifyListeners();
   }
 }
+
+class InputController extends ChangeNotifier{
+  static InputController instance = InputController();
+  String anoNascimento = '';
+  changeName(String value){    
+    anoNascimento = value;
+    notifyListeners();
+  }
+}
+
+class ValidationController extends ChangeNotifier{
+  static ValidationController instance = ValidationController();
+  bool isValid = false;
+
+  final verifyYear = RegExp(r'^[0-9]{4}$');
+  showErrorAlert(BuildContext context){
+    showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: const Text('Erro'),
+          content: const Text('Ano de nascimento inválido'),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar'),
+            )
+          ],
+        );
+      }
+    );
+  }
+  validate(String value, BuildContext context) {
+    if (!(verifyYear.hasMatch(value))) {
+      showErrorAlert(context);
+    } else {
+      if(int.parse(value) > DateTime.now().year ||
+        int.parse(value) < 1900){
+          showErrorAlert(context);
+          return;
+        }
+      isValid = true;
+      notifyListeners();
+    }
+  }
+
+  reset(){
+    isValid = false;
+    notifyListeners();
+  }
+
+  calculateAge(String value){
+    return DateTime.now().year - int.parse(value);
+  }
+}
+
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,58 +1,149 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:ola_mundo/app_controller.dart';
 
-// Usar o StatefulWidget quando precisar alterar o estado do widget
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
+  State<HomePage> createState() => _HomePageState();
 }
 
-class HomePageState extends State<HomePage> {
-  List<String> names = ['Daniel', 'Maria', 'João'];
-  bool isDarkTheme = false;
-  String name = '';
+class _HomePageState extends State<HomePage> {
+  bool isValid = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomePage'),
+        title: const Text('Calculadora de Idade'),
         backgroundColor: Colors.purple[200],
         actions: [
-          CustomSwitch()
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: Row(
+              children: [
+                Text(AppController.instance.isDarkTheme ? 'Dark' : 'Light'),
+                const SizedBox(width: 10),
+                CustomSwitch(),
+              ],
+            ),
+          ),
         ],
       ),
-      body: Center(
-          child: CustomSwitch()
-      ),
-      floatingActionButton: TextButton(
-        onPressed: () {
-          final int randomIndex = Random().nextInt(names.length);
-          setState(() {
-            name = names[randomIndex];
-          });
-        },
-        child: const Text('Gerar nome'),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: double.infinity,
+              color: Colors.cyan,
+              child: Center(
+                child: AgeText(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            CustomInput(),
+            CustomButton(),
+          ],
+        ),
       ),
     );
   }
 }
 
-
-class CustomSwitch extends StatelessWidget{
+class CustomSwitch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Switch(
       value: AppController.instance.isDarkTheme,
-      onChanged: (value){
+      onChanged: (value) {
         AppController.instance.changeTheme();
       },
     );
+  }
+}
+
+class CustomInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.text,
+      onChanged: (value) {
+        InputController.instance.changeName(value);
+      },
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: TextButton(
+        onPressed: () {
+          ValidationController.instance.validate(
+            InputController.instance.anoNascimento,
+            context,
+          );
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.indigoAccent),
+          foregroundColor: MaterialStateProperty.all(Colors.black),
+          padding: MaterialStateProperty.all(const EdgeInsets.all(15)),
+          textStyle: MaterialStateProperty.all(
+            const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        child: const Text('Calcular Idade'),
+      ),
+    );
+  }
+}
+
+class AgeText extends StatefulWidget {
+  @override
+  _AgeTextState createState() => _AgeTextState();
+}
+
+class _AgeTextState extends State<AgeText> {
+  String ageText = 'Insira seu ano de nascimento para calcularmos sua idade';
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      ageText,
+      style: const TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  @override
+  initState() {
+    super.initState();
+    ValidationController.instance.addListener(updateAgeText);
+  }
+
+  @override
+  dispose() {
+    ValidationController.instance.removeListener(updateAgeText);
+    super.dispose();
+  }
+
+  updateAgeText() {
+    setState(() {
+      if (InputController.instance.anoNascimento.isNotEmpty &&
+          ValidationController.instance.isValid) {
+        ageText = 'Sua Idade: ${ValidationController.instance.calculateAge(InputController.instance.anoNascimento)}';
+      } else {
+        ageText = 'Insira seu ano de nascimento para calcularmos sua idade';
+      }
+    });
   }
 }
